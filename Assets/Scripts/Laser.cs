b4443d5ce0b070f165d75548e1cf5804a99f7bbb
Pedro Calle Jaramillo <pedrocalle100@gmail.com>
2017-02-20 10:16:07 -0500

    First Commit
    Acá está su tarea, deben de descargar github. Bajar el el juego. Esto
    les ayudará a saber que es github y como manejarlo, les ayudará mucho en
    su carrera en general. La tarea es que para el viernes creen una nueva
    escena, y en esta deben llegar hasta el punto de tener una nave que se
    mueva y dispare, en adición de algún detalle más que ustedes quieran.
    Mucha suerte

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+	public float speed;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb.velocity = transform.forward * speed;	
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	
+	public float speed;
+	public Rigidbody rb;
+	public Limit limit;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	public float nextFire;
+
+
+	[System.Serializable]
+	public class Limit{
+		public float xMin, zMin, xMax, zMax;
+	}
+
+	//función que genera disparos de la nave
+
+	void Update(){
+		if (Input.GetKey(KeyCode.Space) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	// función que mueve la nave
+
+	void FixedUpdate(){
+		float movementHorizontal = Input.GetAxis ("Horizontal");
+		float movementVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (movementHorizontal, 0.0f, movementVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3(
+			Mathf.Clamp(rb.position.x, limit.xMin, limit.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, limit.zMin, limit.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+
+	}
+}
